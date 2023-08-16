e9a4c90882dd6c5a890a32319afe2a4c24d9b7b8
Amit Tsur <amit.tsur@gmail.com>
2023-08-16 12:58:11 +0300

    Small OwAds fix

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,10 @@
 export * from './promisify';
 export * from './types/overwolf.d.ts';
 export * from './types/owads.d.ts';
+import { OwAd as OwAdClass } from './types/owads.d.ts';
+
+// This fixes the OwAd type not being globally available
+// TODO: Remove this once Overwolf addresses it in their own repo.
+declare global {
+  var OwAd: typeof OwAdClass;
+}
