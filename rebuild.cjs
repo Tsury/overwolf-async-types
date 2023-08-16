c5e4715d87da3260d5d11c3df7fa74806f99fc20
Amit Tsur <amit.tsur@gmail.com>
2023-08-16 14:59:36 +0300

    Fixed rebuilt

--- rebuild.cjs
+++ rebuild.cjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
-exec('npm run build', (error, stdout, stderr) => {
+
+exec('npm run build', { cwd: __dirname }, (error, stdout, stderr) => {
   console.log(stdout);
   console.error(stderr);
 });
